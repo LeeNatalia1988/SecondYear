1bd540a49d5e5ddf2a5f484d61ea056bdc04ddfe
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-03-03 17:41:25 +0700

    HW for Seminar_4 is ok

---
+++ 2 Разработка приложений на C#/Seminar_4/HW/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Linq;
+
+
+namespace HW
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            /*Дан массив и число. Найдите три числа в массиве сумма которых равна искомому числу.
+            Подсказка: если взять первое число в массиве, можно ли найти в оставшейся его части два
+            числа равных по сумме первому.*/
+            double[] array = new double[10] {1, 2, 3, 2, 5, 6, 7, 8, 10, 9};
+            double number = 27;
+            findNumbers1(array, number); // один вариант решения
+            findNumbers2(array, number); // второй вариант
+            findNumbers3(array, number); // третий вариант решения (намешала много чего, хотела попробовать)
+        }
+        public static void findNumbers1(double[] array, double number) {
+            for (int i = 0; i < array.Length; i++)
+            {
+                double temp = number - array[i];
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    double temp1 = temp - array[j];
+                    for (int k = j + 1; k < array.Length; k++)
+                    {
+                        if (array[k] == temp1)
+                        {
+                            Console.WriteLine($"Искомые числа и их индексы: {array[i]} [{i}], {array[j]} [{j}], {array[k]} [{k}].");
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void findNumbers2(double[] array, double number)
+        {
+            var hs = new HashSet<double>();   
+            
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                double temp = number - array[i] - array[i+1];
+                if (hs.Contains(temp))
+                {
+                    Console.WriteLine($"Искомые числа: {array[i]}, {array[i+1]}, {temp}");
+                }
+                else
+                {
+                    hs.Add(array[i]);
+                }
+            }
+        }
+
+        public static void findNumbers3(double[] array, double number)
+        {
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                double temp = number - array[i] - array[i+1];
+                for (int j = i + 2; j < array.Length; j++) 
+                {
+                    IEnumerable<double> numbers = array.Where(x => x == temp);
+                    if (numbers.Count() > 0)
+                    {
+                        Console.Write($"Искомые числа и их индексы: {numbers.ElementAt(0)} [{j}], {array[i]} [{i}], {array[i+1]} [{i+1}].");
+                        
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
+
---
+++ 2 Разработка приложений на C#/Seminar_4/OnSeminar/Program.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace OnSeminar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Дан список целых чисел(числа не последовательны), в котором некоторые числа повторяются.
+            Выведите список чисел на экран, исключив из него повторы. C HashSet и без.
+            List<int> ints = new List<int> { 0, 1, 1, -1, 101, 102, 101, 11, 1111, 11 };
+            
+            HashSet<int> set = new HashSet<int>(ints);
+            List<int> list = new List<int>();
+            Console.Write("{ ");
+            foreach (int i in set)
+            {                
+                Console.Write($"{i}, ");
+            }
+            Console.Write(" }");
+            Console.WriteLine();
+
+            Console.Write("{ ");
+            for (int i = 0; i < ints.Count; i++)
+            {
+                if (!list.Contains(ints[i]))
+                {
+                    list.Add(ints[i]);
+                    Console.Write($"{ints[i]}, ");
+                }
+            }
+            Console.Write(" }");
+            Console.WriteLine();
+
+            List<int> list1 = new List<int>();
+            Console.Write("{ ");
+            foreach (int i in ints)
+            {
+                if (!list1.Any(x => x == i || x == -i)) list1.Add(i); 
+            }
+            foreach (int i in list1)
+            {
+                Console.Write($"{i}, ");
+            }
+            Console.Write(" }");*/
+
+            /*Дан список целых чисел(числа не последовательны), в котором некоторые числа повторяются.
+             * Выведите список чисел на экран, расположив их в порядке возрастания частоты повторения
+            List<int> ints = new List<int> { 1, 2, 2, 2, 3, 4, 4, 5, 5, 5, 5, 6, 7, 0 };*/
+
+            /*List<int> ints = new List<int> { 1, 2, 2, 2, 3, 4, 4, 5, 5, 5, 5, 6, 7, 0 };
+            sortDictionary(ints);
+        }
+
+        public static void sortDictionary(List<int> ints)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            foreach (int i in ints)
+            {
+                if (dic.ContainsKey(i))
+                {
+                    dic[i]++;
+                }
+                else
+                {
+                    dic.Add(i, 1);
+                }
+            }
+            /*Console.WriteLine("Цифра: Количество:");
+            foreach (var d in dic.OrderBy(x => x.Value))
+            {
+                Console.WriteLine($"  {d.Key}    -    {d.Value}");
+            }*/
+
+
+            /*var priorityQueue = new PriorityQueue<int, int>();
+            foreach(var d in dic)
+            {
+                priorityQueue.Enqueue(d.Key, d.Value);
+            }
+            while (priorityQueue.TryDequeue(out var number, out var count))
+            {
+                Console.WriteLine($"Цифра: {number}, Количество: {count}");
+            }
+            
+            var priorityQueue = new PriorityQueue<int, int>();
+            foreach (var d in dic)
+            {
+                priorityQueue.Enqueue(d.Key, -d.Value);
+            }
+            while (priorityQueue.TryDequeue(out var number, out var count))
+            {
+                Console.WriteLine($"Цифра: {number}, Количество: {Math.Abs(count)}");
+            }*/
+
+
+
+
+            /*У вас есть список студентов.Необходимо отфильтровать студентов старше 20 лет и отсортировать их по алфавиту.*/
+            /*Задача: У вас есть список студентов. Необходимо вывести первые 5 студентов и пропустить первых 3 студентов.*/
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Иванов Иван Иванович", 21));
+            students.Add(new Student("Петров Петр Петрович", 20));
+            students.Add(new Student("Сидоров Сидр Сидорович", 23));
+            students.Add(new Student("Кандибобер Бургеркинг", 24));
+            students.Add(new Student("Иванова Мария Андреевна", 18));
+            students.Add(new Student("Иванова Клавдия Петровна", 22));
+            students.Add(new Student("Иванова Клавдия Петровна", 22));
+            students.Add(new Student("Иванова Клавдия Петровна", 22));
+            students.Add(new Student("Иванова Клавдия Петровна", 22));
+            students.Add(new Student("Иванова Клавдия Петровна", 22));
+            /*foreach (Student student in students.OrderBy(x => x.FullName).Where(a => a.Age >= 20))
+            {
+                Console.WriteLine($"Полное имя студента: {student.FullName}, возраст: {student.Age}");
+            }*/
+            
+            /*foreach (Student student in students.Skip(3).Take(5))
+            {
+                Console.WriteLine($"Полное имя студента: {student.FullName}, возраст: {student.Age}");
+            }*/
+        }
+        /*public class Student
+        {
+            public string FullName { get; set; }
+            public int Age { get; set; }
+
+            public Student(string FullName, int Age)
+            {
+                this.FullName = FullName;
+                this.Age = Age;
+            }
+        }*/
+    }
+}
+
