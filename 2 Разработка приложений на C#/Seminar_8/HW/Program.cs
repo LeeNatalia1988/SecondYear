2d5a5af08bd444d4fde84aef74f6c5132ff7e034
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-03-18 17:25:17 +0700

    HW for Seminar_8 is ok

---
+++ 2 Разработка приложений на C#/Seminar_8/HW/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+
+namespace HW
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            MySearchAddressFilesWithWord(args[0], args[1], args[2]);
+        }
+        
+        static List<string> listAddress = new List<string>();
+        public static void MySearchAddressFilesWithWord(string path, string extension, string word)
+        {
+            /*Объедините две предыдущих работы(практические работы 2 и 3): поиск файла и поиск текста в файле написав 
+             * утилиту которая ищет файлы определенного расширения с указанным текстом.Рекурсивно.
+             * Пример вызова утилиты: utility.exe txt текст.*/
+            //PS: Все параметры, передаваемые в args нужно брать в "", если есть пробелы (в особенности в адресе)
+
+            listAddress.AddRange(Directory.GetFiles(path, "*." + extension, SearchOption.AllDirectories));
+            foreach (string file in listAddress)
+            {
+                using (var sr = new StreamReader(file))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var temp = sr.ReadLine();
+                        if (temp.Contains(word))
+                        {
+                            Console.WriteLine(file);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 2 Разработка приложений на C#/Seminar_8/OnSeminar/Program.cs
@@ -0,0 +1,99 @@
+﻿
+using System.ComponentModel.Design;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace OnSeminar
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //MyUtility(args);
+            /*MySearchAddress(args[0], args[1]); 
+            foreach (string ls in listAddress) {
+                Console.WriteLine(ls);
+            }*/
+            MySearchStringWithWord(args[0], args[1]);
+
+        }
+        public static void MySearchStringWithWord(string fileName, string word)
+        {
+            /*Напишите утилиту читающую тестовый файл и выводящую на экран строки содержащие искомое слово.*/
+            using (var sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var temp = sr.ReadLine();
+                    if (temp.Contains(word))
+                    {
+                        Console.WriteLine(temp);
+                    }
+                }
+            }
+        }
+
+        public static void MyUtility(string[] args)
+        {
+            /* Напишите консольную утилиту для копирования файлов
+            Пример запуска: utility.exe file1.txt file2.txt*/
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Вы ввели неверные данные.");
+            }
+            else
+            {
+                if (!File.Exists(args[0]))
+                {
+                    using (var sw = new StreamWriter(args[0]))
+                    {
+                        sw.WriteLine("Что-нибудь.");
+                        Console.WriteLine("Файл создан.");
+                    }
+                }
+
+                using (var sr = new StreamReader(args[0]))
+                {
+                    using (var sw = new StreamWriter(args[1]))
+                    {
+                        sw.WriteLine(sr.ReadToEnd());
+                    }
+                }
+                Console.WriteLine("Файл скопирован.");
+            }
+        }
+
+        static List<string> listAddress = new List<string>();
+        public static void MySearchAddress(string path, string name)
+        {
+            /*Напишите утилиту рекурсивного поиска файлов в заданном каталоге и подкаталогах
+            Габиль Асланов Пример запуска: utility.exe c:\t file1.txt*/
+
+            listAddress.AddRange(Directory.GetFiles(path, name, SearchOption.AllDirectories));
+            /*var allFiles = Directory.GetFiles(path);
+            
+            foreach(var file in allFiles)
+            {
+                if(Path.GetFileName(file) == name)
+                {
+                    listAddress.Add(file);
+                }
+            }
+            foreach(var dir in Directory.GetDirectories(path))
+            {
+                MySearch(dir, name);
+            }*/
+
+        }
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
