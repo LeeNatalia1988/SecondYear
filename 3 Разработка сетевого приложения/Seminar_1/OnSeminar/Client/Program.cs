763f8fab08560b86ac3d05be1b48e2430c4d5bd5
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-03-22 17:20:24 +0700

    Code from Seminar_1 added1

---
+++ 3 Разработка сетевого приложения/Seminar_1/OnSeminar/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Server;
+
+namespace OnSeminar
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string ip = args[1];
+            string from = args[0];
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(ip), 1234);
+
+
+            while (true)
+            {
+                string? messageText;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Введите сообщение: ");
+                    messageText = Console.ReadLine();
+                }
+                while (string.IsNullOrEmpty(messageText));
+
+                Message message = new Message() { Text = messageText, DateTime = DateTime.Now, NickNameFrom = from, NickNameTo = "Server" };
+                string json = message.SerializeMessageToJson();
+
+                byte[] data = Encoding.UTF8.GetBytes(json);
+                udpClient.Send(data, data.Length, ipEndPoint);
+            }
+        }
+    }
+}
