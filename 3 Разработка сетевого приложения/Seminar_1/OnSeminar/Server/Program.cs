4cc542994e95364c225653412588191406d2d3b3
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-03-22 17:18:43 +0700

    Code from Seminar_1 added

---
+++ 3 Разработка сетевого приложения/Seminar_1/OnSeminar/Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OnSeminar
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient(1234);
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Сервер ждет сообщение от клиента...");
+
+            while (true)
+            {
+                byte[] buffer = udpClient.Receive(ref ipEndPoint);
+                if (buffer == null) break;
+                var messageText = Encoding.UTF8.GetString(buffer);
+                Message? messageServer = Message.DeserializeFromJsonToMessage(messageText);
+                messageServer?.PrintMessage();
+            }
+        }
+    }
+}
