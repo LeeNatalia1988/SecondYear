b5fe7656da901c9afbd8027ba42f63e9ee6f3953
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-04-08 17:00:07 +0700

    HW for Seminar_3 is ok

---
+++ 3 Разработка сетевого приложения/Seminar_3/HW/SocketChat/SocketChat.DAL.Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using SocketChat.Common.Entities;
+
+namespace SocketChat.DAL.Repositories
+{
+    public static class UserRepository
+    {
+        private static List<User> users = new List<User>();
+
+        public static List<User> GetAll()
+        {
+            return users;
+        }
+
+        public static async void AddUser(User user)
+        {
+            await Task.Delay(1000);
+            users.Add(user);
+        }
+    }
+}
