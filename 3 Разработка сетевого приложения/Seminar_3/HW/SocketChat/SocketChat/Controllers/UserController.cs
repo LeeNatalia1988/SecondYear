dfe8ebbe9a393208178bc1fa72fdb84d4655dfa3
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-04-08 16:58:02 +0700

    HW for Seminar_4 is ok

---
+++ 3 Разработка сетевого приложения/Seminar_3/HW/SocketChat/SocketChat/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocketChat.BLL.Logic;
+using SocketChat.Common.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SocketChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserLogic _userLogic;
+
+        public UserController(IUserLogic userLogic)
+        {
+            _userLogic = userLogic;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public async IAsyncEnumerable<User> Get()
+        {
+            await Task.Delay(1000);
+            var users = _userLogic.GetAll();
+
+            var user = users.FirstOrDefault(u => u.Name == "Bob");
+            foreach (var u in users)
+            {
+                if (u != null)
+                {
+                    yield return u;
+                }
+            }
+            
+        }
+        
+        // POST api/<UserController>
+        [HttpPost]
+        public async void Post([FromBody] User user)
+        {
+            await Task.Delay(1000);
+            _userLogic.Add(user);
+        }
+    }
+}
