4bebbb7abed36505cd79c6b1869ba8755525b936
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-04-12 19:29:04 +0700

    HW for Seminar_4 is ok

---
+++ 3 Разработка сетевого приложения/Seminar_4/HW/SocketChat/SocketChat.BLL.Logic/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SocketChat.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketChat.BLL.Logic
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string username, string message)
+        {
+            await this.Clients.All.SendAsync("Receive", username, message);
+        }
+    }
+}
---
+++ 3 Разработка сетевого приложения/Seminar_4/HW/SocketChatClient/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Windows;
+
+namespace SocketChatClient
+{
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder().WithUrl("https://localhost:7217/chat").Build();
+
+            connection.On<string, string>("Receive", (user, message) =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    chatbox.Items.Insert(0, newMessage);
+                });
+            });
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.StartAsync();
+                chatbox.Items.Add("Вы вошли в чат");
+                sendBtn.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                chatbox.Items.Add(ex.Message);
+            }
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("Send", userTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                chatbox.Items.Add(ex.Message);
+            }
+        }
+    }
+}
