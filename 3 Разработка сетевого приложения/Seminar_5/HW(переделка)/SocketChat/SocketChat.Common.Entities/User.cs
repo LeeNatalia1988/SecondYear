9a0dc9b111396a3496d9ae56c0e7cb50e827cd28
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-04-20 21:31:40 +0700

    HW for Seminar_5 was corrected

---
+++ 3 Разработка сетевого приложения/Seminar_5/HW(переделка)/SocketChat/SocketChat.Common.Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SocketChat.Common.Entities
+{
+    [Table("users")]
+    public partial class User
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+        
+        [Column("name")]
+        public string Name { get; set; }
+        
+        [Column("password")]
+        public string Password { get; set; }
+        
+        [Column("email")]
+        public string? Email { get; set; }
+        
+        public virtual ICollection<SignalRMessage>? Messages { get; set;}
+    }
+}
