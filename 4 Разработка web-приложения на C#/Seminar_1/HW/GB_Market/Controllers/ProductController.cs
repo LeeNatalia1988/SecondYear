15aef320d75e6e90cddac3f98ec0608efd4fbfa4
Lee_Natalia <mosaleva.lee.natalia1988@gmail.com>
2024-05-22 12:47:22 +0700

    HW for Seminar_1 is ok

---
+++ 4 Разработка web-приложения на C#/Seminar_1/HW/GB_Market/Controllers/ProductController.cs
@@ -0,0 +1,108 @@
+﻿using GB_Market.DB;
+using GB_Market.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GB_Market.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Components.Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost(template: "add_product")]
+        public ActionResult AddProduct(string name, string description)
+        {
+            try
+            {
+                using (var ctx = new ProductContext())
+                {
+                    if (ctx.Products.Count(x => x.Name.ToLower() == name.ToLower()) > 0)
+                    {
+                        return StatusCode(409);
+                    }
+                    else
+                    {
+                        ctx.Products.Add(new Product { Name = name, Description = description});
+                        ctx.SaveChanges();
+                    }
+                }
+
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpGet(template: "get_products")]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            try
+            {
+                using (var ctx = new ProductContext())
+                {
+                    var list = ctx.Products.Select(x => new Product { Id = x.Id, Name = x.Name, Description = x.Description }).ToList();
+                    return list;
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost(template: "delete_product")]
+        public ActionResult DeleteProduct(string name)
+        {
+            try
+            {
+                using (var ctx = new ProductContext())
+                {
+                    if (ctx.Products.Count(x => x.Name.ToLower() == name.ToLower()) == 0)
+                    {
+                        return StatusCode(406);
+                    }
+                    else
+                    {
+                        var product = ctx.Products.Where(x => x.Name.ToLower() == name.ToLower()).FirstOrDefault();
+                        ctx.Products.Remove(product);
+                        ctx.SaveChanges();
+                    }
+                }
+
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost(template: "patch_product_description")]
+        public ActionResult PatchProduct(string name, string description)
+        {
+            try
+            {
+                using (var ctx = new ProductContext())
+                {
+                    if (ctx.Products.Count(x => x.Name.ToLower() == name.ToLower()) == 0)
+                    {
+                        return StatusCode(406);
+                    }
+                    else
+                    {
+                        var product = ctx.Products.Where(x => x.Name.ToLower() == name.ToLower()).FirstOrDefault();
+                        product.Description = description;
+                        ctx.SaveChanges();
+                    }
+                }
+
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ 4 Разработка web-приложения на C#/Seminar_1/HW/GB_Market/Controllers/ProductGroupController.cs
@@ -0,0 +1,109 @@
+﻿using GB_Market.DB;
+using GB_Market.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GB_Market.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Components.Route("[controller]")]
+    public class ProductGroupController : ControllerBase
+    {
+        [HttpPost(template: "add_group")]
+        public ActionResult AddGroup(string name, string description)
+        {
+            try
+            {
+                using (var ctx = new ProductContext())
+                {
+                    if (ctx.ProductGroup.Count(x => x.Name.ToLower() == name.ToLower()) > 0)
+                    {
+                        return StatusCode(409);
+                    }
+                    else
+                    {
+                        ctx.ProductGroup.Add(new ProductGroup { Name = name, Description = description });
+                        ctx.SaveChanges();
+                    }
+                }
+
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpGet(template: "get_product_group")]
+        public ActionResult<IEnumerable<ProductGroup>> GetProductGroup()
+        {
+            try
+            {
+                using (var ctx = new ProductContext())
+                {
+                    var list = ctx.ProductGroup.Select(x => new ProductGroup { Id = x.Id, Name = x.Name, Description = x.Description }).ToList();
+                    return list;
+                }
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost(template: "delete_product_group")]
+        public ActionResult DeleteProductGroup(string name)
+        {
+            try
+            {
+                using (var ctx = new ProductContext())
+                {
+                    if (ctx.ProductGroup.Count(x => x.Name.ToLower() == name.ToLower()) == 0)
+                    {
+                        return StatusCode(406);
+                    }
+                    else
+                    {
+                        var productGroup = ctx.ProductGroup.Where(x => x.Name.ToLower() == name.ToLower()).FirstOrDefault();
+                        ctx.ProductGroup.Remove(productGroup);
+                        ctx.SaveChanges();
+                    }
+                }
+
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost(template: "patch_product_group_description")]
+        public ActionResult PatchProductGroupDescription(string name, string description)
+        {
+            try
+            {
+                using (var ctx = new ProductContext())
+                {
+                    if (ctx.ProductGroup.Count(x => x.Name.ToLower() == name.ToLower()) == 0)
+                    {
+                        return StatusCode(406);
+                    }
+                    else
+                    {
+                        var productGroup = ctx.ProductGroup.Where(x => x.Name.ToLower() == name.ToLower()).FirstOrDefault();
+                        productGroup.Description = description;
+                        ctx.SaveChanges();
+                    }
+                }
+
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
+
+
